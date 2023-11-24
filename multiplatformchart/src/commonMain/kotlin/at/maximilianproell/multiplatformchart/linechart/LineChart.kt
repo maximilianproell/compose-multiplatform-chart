370bcd18eddc84fc5b88c445aaa3aff6a4ba8873
Maximilian Pröll <maximilian.proell@bitsfabrik.com>
2023-11-24 22:18:23 +0100

    - Added ability to LineChart to zoom and pan with gestures

--- multiplatformchart/src/commonMain/kotlin/at/maximilianproell/multiplatformchart/linechart/LineChart.kt
+++ multiplatformchart/src/commonMain/kotlin/at/maximilianproell/multiplatformchart/linechart/LineChart.kt
@@ -2,6 +2,7 @@ package at.maximilianproell.multiplatformchart.linechart
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,7 +16,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
@@ -26,6 +30,8 @@ import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.inset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -34,12 +40,12 @@ import at.maximilianproell.multiplatformchart.common.LabeledXAxisConfig
 import at.maximilianproell.multiplatformchart.common.LabeledYAxisConfig
 import at.maximilianproell.multiplatformchart.common.drawXAxisWithLabels
 import at.maximilianproell.multiplatformchart.common.drawYAxisWithLabels
-import at.maximilianproell.multiplatformchart.linechart.utils.dataToOffSet
 import at.maximilianproell.multiplatformchart.linechart.config.LineConfig
 import at.maximilianproell.multiplatformchart.linechart.config.LineConfigDefaults
 import at.maximilianproell.multiplatformchart.linechart.model.LegendEntry
 import at.maximilianproell.multiplatformchart.linechart.model.LineDataSet
 import at.maximilianproell.multiplatformchart.linechart.model.toLegendEntry
+import at.maximilianproell.multiplatformchart.linechart.utils.dataToOffSet
 
 @Composable
 fun LineChart(
@@ -55,6 +61,24 @@ fun LineChart(
 ) {
     val textMeasurer = rememberTextMeasurer()
 
+
+    // We assume that the list of lineData points is sorted. Therefore, it is sufficient to get
+    // the first and last value.
+    var minXLineData by remember {
+        mutableStateOf(
+            lineDataSets().minOfOrNull { it.dataPoints.firstOrNull()?.xValue ?: 0f } ?: 0f
+        )
+    }
+    var maxXLineData by remember {
+        mutableStateOf(
+            lineDataSets().maxOfOrNull { it.dataPoints.lastOrNull()?.xValue ?: 0f } ?: 0f
+        )
+    }
+
+    var canvasWidth by remember {
+        mutableStateOf(1)
+    }
+
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -64,15 +88,32 @@ fun LineChart(
                 .weight(1f)
                 .fillMaxWidth()
                 .clipToBounds()
+                .pointerInput(Unit) {
+                    detectTransformGestures { centroid, pan, zoom, rotation ->
+                        val originalDifference = maxXLineData - minXLineData
+                        val zoomedRangeLength = originalDifference * zoom
+                        val difference = originalDifference - zoomedRangeLength
+
+                        val centerWeighting = centroid.x / canvasWidth
+
+                        maxXLineData += difference * (1 - centerWeighting)
+                        minXLineData -= difference * centerWeighting
+
+                        if (originalDifference != 0f) {
+                            val moveDifference = pan.x * (originalDifference / canvasWidth)
+                            maxXLineData -= moveDifference
+                            minXLineData -= moveDifference
+                        }
+                    }
+                }
+                .onGloballyPositioned {
+                    canvasWidth = it.size.width
+                }
         ) {
             val circleRadiusPx = if (lineConfig.showLineDots) circleRadius.toPx() else 0f
             val yAxisLabelsOffsetPx = yAxisConfig.labelsXOffset.toPx()
             val yPaddingOffset = if (yAxisLabelsOffsetPx < 0f) yAxisLabelsOffsetPx * -1 else 0f
 
-            // We assume that the list of lineData points is sorted. Therefore, it is sufficient to get
-            // the first and last value.
-            val minXLineData = lineDataSets().minOfOrNull { it.dataPoints.firstOrNull()?.xValue ?: 0f } ?: 0f
-            val maxXLineData = lineDataSets().maxOfOrNull { it.dataPoints.lastOrNull()?.xValue ?: 0f } ?: 0f
 
             val xAxisLabelsYOffsetPx = xAxisConfig.labelsYOffset.toPx()
             val bottomLabelPadding = if (xAxisLabelsYOffsetPx > 0) {
