049a0b3e7368db47edc8cfe482c8cbd5bf504cf4
Maximilian Pröll <maximilian.proell@bitsfabrik.com>
2023-12-06 14:04:14 +0100

    - Removed basic downSample function from DataPoint since it's practically useless

--- multiplatformchart/src/commonMain/kotlin/at/maximilianproell/multiplatformchart/linechart/model/DataPoint.kt
+++ multiplatformchart/src/commonMain/kotlin/at/maximilianproell/multiplatformchart/linechart/model/DataPoint.kt
@@ -1,8 +1,5 @@
 package at.maximilianproell.multiplatformchart.linechart.model
 
-import androidx.compose.ui.util.fastForEachIndexed
-import kotlin.math.ceil
-
 /**
  * Represents a data point in the line chart.
  */
@@ -13,18 +10,4 @@ data class DataPoint(val xValue: Float, val yValue: Float)
  */
 fun List<DataPoint>.filterInVisibleRange(minXData: Float, maxXData: Float): List<DataPoint> {
     return filter { it.xValue in minXData..maxXData }
-}
-
-// TODO: optimize; very basic implementation for testing
-// TODO: move, since this is a generic implementation
-fun <T> List<T>.downSample(maxValues: Int): List<T> {
-    if (size <= maxValues) return this
-
-    val stepSize = ceil(size / maxValues.toFloat()).toInt()
-
-    return buildList {
-        this@downSample.fastForEachIndexed { index, value ->
-            if (index % stepSize == 0 || index == lastIndex) add(value)
-        }
-    }
 }
